4a5fd3b64633c0ac2372af8c21784d9e4a71bdf9
Md. Saimon Hasan Emon <saimonemon46@gmail.com>
2026-05-08 18:08:52 +0600

    user api connected

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from src.incidents.controller import router as incidents_router
+from src.users.controller import router as users_router
 
 api_router = APIRouter(prefix="/api/v1")
 
+api_router.include_router(users_router)
 api_router.include_router(incidents_router)
