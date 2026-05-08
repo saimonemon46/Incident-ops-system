a2d863069fb0c63d8c039296b74d6ba9b345eaa7
Md. Saimon Hasan Emon <saimonemon46@gmail.com>
2026-05-08 17:22:36 +0600

    api.py

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from src.incidents.controller import router as incidents_router
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/api/v1")
+
 api_router.include_router(incidents_router)
