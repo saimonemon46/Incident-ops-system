b1ad4171c8a213959eaaf2a5eb1f83a115807e8f
Md. Saimon Hasan Emon <saimonemon46@gmail.com>
2026-05-15 00:57:49 +0600

    application routing and layout flow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import AppRouter from './app/router';
+import { QueryProvider } from './app/providers/QueryProvider';
+import { SocketProvider } from './app/providers/SocketProvider';
+import { ThemeProvider } from './app/providers/ThemeProvider';
+import './styles/globals.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <QueryProvider>
+        <SocketProvider>
+          <AppRouter />
+        </SocketProvider>
+      </QueryProvider>
+    </ThemeProvider>
   );
 }
 
